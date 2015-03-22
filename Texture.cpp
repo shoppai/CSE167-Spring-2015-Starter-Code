e111014cc2adce1ba5873c06ca4bc20045a0f9bc
Rex West <rexwest@gmail.com>
2015-03-22 12:03:21 +0900

    Revisions Draft 8 - Re-formatted Texture.loadPPM()

--- Texture.cpp
+++ Texture.cpp
@@ -76,53 +76,53 @@ void Texture::unbind(void)
  **/
 unsigned char* Texture::loadPPM(const char* filename, int& width, int& height)
 {
-	const int BUFSIZE = 128;
-	FILE* fp;
-	size_t read;
-	unsigned char* rawData;
-	char buf[3][BUFSIZE];
-	char* retval_fgets;
-	size_t retval_sscanf;
+    const int BUFSIZE = 128;
+    FILE* fp;
+    size_t read;
+    unsigned char* rawData;
+    char buf[3][BUFSIZE];
+    char* retval_fgets;
+    size_t retval_sscanf;
     
-	if ( (fp=fopen(filename, "rb")) == NULL)
-	{
-		std::cerr << "error reading ppm file, could not locate " << filename << std::endl;
-		width = 0;
-		height = 0;
-		return NULL;
-	}
+    if((fp=fopen(filename, "rb")) == NULL)
+    {
+        std::cerr << "error reading ppm file, could not locate " << filename << std::endl;
+        width = 0;
+        height = 0;
+        return NULL;
+    }
     
-	// Read magic number:
-	retval_fgets = fgets(buf[0], BUFSIZE, fp);
+    // Read magic number:
+    retval_fgets = fgets(buf[0], BUFSIZE, fp);
     
-	// Read width and height:
-	do
-	{
-		retval_fgets=fgets(buf[0], BUFSIZE, fp);
-	} while (buf[0][0] == '#');
-    
-	retval_sscanf=sscanf(buf[0], "%s %s", buf[1], buf[2]);
-	width  = atoi(buf[1]);
-	height = atoi(buf[2]);
-    
-	// Read maxval:
-	do
-	{
+    // Read width and height:
+    do
+    {
         retval_fgets=fgets(buf[0], BUFSIZE, fp);
-	} while (buf[0][0] == '#');
+    } while (buf[0][0] == '#');
     
-	// Read image data:
-	rawData = new unsigned char[width * height * 3];
-	read = fread(rawData, width * height * 3, 1, fp);
-	fclose(fp);
-	if (read != 1)
-	{
-		std::cerr << "error parsing ppm file, incomplete data" << std::endl;
-		delete[] rawData;
-		width = 0;
-		height = 0;
-		return NULL;
-	}
+    retval_sscanf=sscanf(buf[0], "%s %s", buf[1], buf[2]);
+    width  = atoi(buf[1]);
+    height = atoi(buf[2]);
     
-	return rawData;
+    // Read maxval:
+    do
+    {
+        retval_fgets=fgets(buf[0], BUFSIZE, fp);
+    } while (buf[0][0] == '#');
+    
+    // Read image data:
+    rawData = new unsigned char[width * height * 3];
+    read = fread(rawData, width * height * 3, 1, fp);
+    fclose(fp);
+    if (read != 1)
+    {
+        std::cerr << "error parsing ppm file, incomplete data" << std::endl;
+        delete[] rawData;
+        width = 0;
+        height = 0;
+        return NULL;
+    }
+    
+    return rawData;
 }
